92160c7ef4f8bba432d6e366f72d3a562180a860
rohan <deepakrohan@hotmail.com>
2021-09-30 16:27:33 -0400

    categories table with exp codes

--- src/main/resources/db/migration/V1_0_2__categories_create.sql
+++ src/main/resources/db/migration/V1_0_2__categories_create.sql
@@ -1,12 +1,12 @@
-create categories {
-    category_id integer not null unique sequence,
+CREATE TABLE IF NOT EXISTS categories (
+    category_id integer not null auto_increment,
     category_name varchar (255),
     category_description text,
-    category_limit decimal(),
+    category_limit decimal(15,2),
     category_added timestamp,
     created_at timestamp not null,
     updated_at timestamp not null,
     created_by varchar(255),
     updated_by varchar(255),
     primary key (category_id)
-}
+)
